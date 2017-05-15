5b71fdc95fc412cd9a9635bdeb10fad542ccd198
Jason Brown <jason@silex-consulting.com>
2017-05-15 23:46:58 +0100

    Add more python packages.

--- provision.sh
+++ provision.sh
@@ -1,3 +1,4 @@
 apt-get --yes --force-yes update
 apt-get --yes --force-yes autoremove
-apt-get --yes --force-yes install python
+apt-get --yes --force-yes install python python3 python-pip python-virtualenv vim vim-python-jedi python-setuptools pype python-numpy python-scipy
+
