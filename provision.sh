cebc22f36c2a7405dc8cf15bff76f9abc19045b3
Jason Brown <jason@silex-consulting.com>
2017-05-15 23:54:41 +0100

    Hello, World!

--- provision.sh
+++ provision.sh
@@ -1,4 +1,5 @@
 apt-get --yes --force-yes update
 apt-get --yes --force-yes autoremove
 apt-get --yes --force-yes install python python3 python-pip python-virtualenv vim vim-python-jedi python-setuptools pype python-numpy python-scipy
-
+echo "print 'Hello, World!'" >> /tmp/hello.py
+python /tmp/hello.py
